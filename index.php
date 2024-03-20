51def9d35e27d794b3b5c6c7c0cc9ed433b67cae
BagasYudha <158305334+BagasYudha@users.noreply.github.com>
2024-03-20 11:22:23 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>YudhaPHP</title>
+    <link rel="icon" type="image/x-icon" href="Media/Profile Pict.png">
+</head>
+
+<body>
+    <?php include "layout/navbar.html" ?>
+    <h2>-------------------</h2>
+
+</body>
+
+</html>
